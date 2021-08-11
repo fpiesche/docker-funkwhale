2fe0ba3d5e2d7be6ce0a259ac380cfbf3fdf52fe
Florian Piesche <florian@yellowkeycard.net>
2021-08-11 19:37:45 +0100

    Add some progress info to get-s6.sh

--- get-s6.sh
+++ get-s6.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-ARCH=$(uname -m)
-wget https://github.com/just-containers/s6-overlay/releases/download/${S6_RELEASE}/s6-overlay-${ARCH}.tar.gz /tmp
-tar xzf /tmp/s6-overlay-${ARCH}.tar.gz -C
+ARCH=`uname -m`
+S6_URL="https://github.com/just-containers/s6-overlay/releases/download/${S6_RELEASE}/s6-overlay-${ARCH}.tar.gz"
+
+echo "Getting ${S6_URL}..."
+wget ${S6_URL} /tmp
+
+echo "Extracting S6 overlay..."
+tar xzf /tmp/s6-overlay-${ARCH}.tar.gz -C /
+rm /tmp/s6-overlay-${ARCH}.tar.gz
