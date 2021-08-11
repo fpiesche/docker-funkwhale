88a5a35bf3182788995824a6acad714b010246d3
Florian Piesche <florian@yellowkeycard.net>
2021-08-11 20:43:24 +0100

    Fix wget call for alpine

--- get-s6.sh
+++ get-s6.sh
@@ -23,7 +23,7 @@ esac
 S6_URL="https://github.com/just-containers/s6-overlay/releases/download/${S6_RELEASE}/s6-overlay-${ARCH}.tar.gz"
 
 echo "Getting ${S6_URL}..."
-wget ${S6_URL} /tmp
+wget ${S6_URL} -P /tmp
 
 echo "Extracting S6 overlay..."
 tar xzf /tmp/s6-overlay-${ARCH}.tar.gz -C /
