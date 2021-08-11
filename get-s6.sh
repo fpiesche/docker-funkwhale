93219255bebe812ece31c7d17df67e9cd17367f5
Florian Piesche <florian@yellowkeycard.net>
2021-08-11 19:54:14 +0100

    Properly parse kernel-reported architecture into S6 arch tags

--- get-s6.sh
+++ get-s6.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
-ARCH=`uname -m`
+KARCH=`uname -m`
+
+case $KARCH in
+    aarch64)
+        ARCH="aarch64"
+        ;;
+    x86_64)
+        ARCH="amd64"
+        ;;
+    armv7l)
+        ARCH="arm"
+        ;;
+    armv6l)
+        ARCH="armhf"
+        ;;
+    *)
+        echo "Unknown architecture $KARCH!"
+        exit 1
+        ;;
+esac
+
 S6_URL="https://github.com/just-containers/s6-overlay/releases/download/${S6_RELEASE}/s6-overlay-${ARCH}.tar.gz"
 
 echo "Getting ${S6_URL}..."
